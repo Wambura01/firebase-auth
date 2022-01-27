88fbc0c749101297d13d46e2f960d0364f0655a3
brian-wambura <brian.partner@space44.com>
2022-01-27 19:07:49 +0300

    Creating a React app with firebase authentication

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+
+import Form from "./components/Common/Form";
 
 function App() {
+  //state
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  //handle action
+  const handleAction = (id) => {
+    console.log(id);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route
+          path="/login"
+          element={
+            <Form
+              title="Login"
+              setEmail={(e) => setEmail(e.target.target)}
+              handleAction={() => handleAction(1)}
+            />
+          }
+        />
+        <Route
+          path="/register"
+          element={
+            <Form
+              title="Register"
+              setPassword={(e) => setPassword(e.target.target)}
+              handleAction={() => handleAction(2)}
+            />
+          }
+        />
+      </Routes>
     </div>
   );
 }
